da4ee9df3e434dc8c1b64a4c1de8206c94c8a00c
Brian Boyd <bboyd019@yahoo.com>
2014-12-19 23:26:38 -0600

    folder layout updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
-// var uglify = require('gulp-uglify');
+var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
 var traceur = require('gulp-traceur');
 var jshint = require('gulp-jshint');
 var jshint_stylish = require('jshint-stylish');
 var wrap = require('gulp-wrap');
+var browsersync = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+	browsersync({
+		proxy: 'localhostd',
+		port: 3000
+	});
+});
+
+
+gulp.task('browsersync-reload', function () {
+    browsersync.reload();
+});
 
 gulp.task('js', function () {
   gulp.src(['src/public/**/app.module.js', 'src/public/**/*.module.js', 'src/public/**/*.js'])
@@ -15,10 +28,11 @@ gulp.task('js', function () {
     .pipe(sourcemaps.init())    
     .pipe(traceur())
     .pipe(concat('app.js'))
-    .pipe(ngAnnotate())
-    // .pipe(uglify())
+    .pipe(ngAnnotate())    
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist/public/js'))
+    .pipe(wrap('(function(){\n"use strict";\n<%= contents %>\n})();'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/public/js'))    
 });
 
 gulp.task('watch', ['js'], function () {
